8f15f5f941b1e35edf3e085351d0799c4071809d
Tre <94414702+Tre0207@users.noreply.github.com>
2022-03-27 14:48:48 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,10 +2,6 @@ import os
 # -*- coding: utf-8 -*-
 
 """
-Copyright (c) 2019 Valentin B.
-
-A simple music bot written in discord.py using youtube-dl.
-
 Though it's a simple example, music bots are complex and require much time and knowledge until they work perfectly.
 Use this as an example or a base for your own bot and extend it as you want. If there are any bugs, please let me know.
 
