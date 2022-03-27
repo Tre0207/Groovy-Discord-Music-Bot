ddb928126a14ff65ebce8252bb15a1497e76edb8
Tre <94414702+Tre0207@users.noreply.github.com>
2022-03-27 14:49:25 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,9 +2,6 @@ import os
 # -*- coding: utf-8 -*-
 
 """
-Though it's a simple example, music bots are complex and require much time and knowledge until they work perfectly.
-Use this as an example or a base for your own bot and extend it as you want. If there are any bugs, please let me know.
-
 Requirements:
 
 Python 3.5+
